b26b3c2262ef00ae38285b2e10d71e336cea5511
ARZATE-BELLO-SERGIO-SACHIEL <sarzateb1900@alumno.ipn.mx>
2021-11-02 21:30:25 -0600

    Cifrado RSA

---
+++ RSA1/src/RSA/PRINCIPAL.java
@@ -0,0 +1,21 @@
+package RSA;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ */
+public class PRINCIPAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interfaz principal = new Interfaz();
+        principal.setVisible(true);
+    }
+    
+}
